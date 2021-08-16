a163ffd96d79125b49eb796f0e0a6e4701515495
James Tarpey <hello@jamestarpey.com>
2021-08-16 22:15:05 +0100

    Update config for Vercel

--- app.js
+++ app.js
@@ -293,6 +293,7 @@ app.get("/table", (req, res, next) => {
   ]);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
